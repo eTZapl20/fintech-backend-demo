913a2c83bd8439b69f3e5edd150f79df8c283770
Dominic Goh <hong1hiem2@gmail.com>
2021-06-30 16:41:58 +0800

    First commit

---
+++ recap.js
@@ -0,0 +1,77 @@
+// x = "Dixant";
+
+// console.log("My message");
+
+// console.log(x);
+
+let x = 12;
+let y = 3;
+
+console.log(
+  "Value of x: " + x + ", Value of y: " + y + ", and " + x + "/" + y + " = ",
+  +(x / y)
+);
+
+console.log(`Value of x: ${x}, Value of y: ${y}, and ${x}/${y} = ${x / y}`);
+
+// console.log(`Value of x : ${x}`);
+// console.log("Value of x : " + x);
+
+// // Conditional Statements
+// let age = 10;
+// if (age > 18) {
+//   console.log("You can vote!");
+// } else {
+//   console.log("You cannot vote right now.");
+// }
+// Functions
+function print_sum(a, b) {
+    console.log(`a: ${a}`);
+    console.log(`b: ${b}`);
+  
+    console.log(`a + b = ${a + b}`);
+  }
+  
+  print_sum(2, 3);
+
+  function add(a, b) {
+    return a + b;
+  }
+  
+  var sum = add(2, 3);
+  
+  console.log(`Sum: ${add(2,3)}`);
+
+  // anonymous functions
+var function_to_print_sum = function (a, b) {
+    console.log("a + b = " + (a + b));
+  };
+  
+  function_to_print_sum(4, 4);
+
+ // Arrow functions
+var function_to_print_sum = (a, b) => {
+    console.log("a + b = " + (a + b));
+  };
+  
+  function_to_print_sum(4, 4); 
+
+  // callback functions
+  function print_after_operation(a, b, operation) {
+    var result = operation(a, b);
+    console.log("Result: " + result);
+  }
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function product(a, b) {
+    return a * b;
+  }
+  
+print_after_operation(3, 4, add);
+print_after_operation(3, 4, product);
+print_after_operation(3, 4, (a, b) => {
+    return a - b;
+  });
